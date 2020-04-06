335542f61a0dcc2c3f8d4b90de0409542979b728
Leonardo Mocka <leonardo@mocka.email>
2020-04-06 09:30:48 -0300

    update

---
+++ swiftuitutotwo/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftuitutotwo
+//
+//  Created by Leonardo Reis on 03/04/20.
+//  Copyright © 2020 Leonardo Reis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
